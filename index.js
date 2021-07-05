081548008a2ee750cc4ad4a08711fe9b3967a83f
wafachaari <wafachaari30@gmail.com>
2021-07-05 15:16:54 +0200

    updated

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ import App from "./App";
 class Portfolio extends React.Component {
   render() {
     return (
-      <div>
+      
         <App />
-      </div>
+      
     );
   }
 }
 
-// Find the root of our app
-const container = document.getElementsByClassName('app-container')[0];
 
-// Tell React to render our app in the root DOM element
+const container = document.getElementsByClassName('app-container')[0];
+ 
 ReactDOM.render(React.createElement(Portfolio), container);
