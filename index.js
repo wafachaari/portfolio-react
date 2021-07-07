57eeb230b48ad297416df7b8078fe0b4b6d9ce5f
wafachaari <wafachaari30@gmail.com>
2021-07-07 10:18:25 +0200

    updated

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Container from 'react-bootstrap/Container';
+ 
+// import { Provider } from 'react-redux';
+// import App from "./App";
+
+// class Portfolio extends React.Component {
+//   render() {
+//     return (
+//       <Provider  >
+//       <Container>
+//         <App/>
+//         </Container>
+//       </Provider>
+//     );
+//   }
+// }
+// // Find the root of our app
+// const container = document.getElementsByClassName('app-container')[0];
+// // Tell React to render our app in the root DOM element
+// ReactDOM.render(React.createElement(Portfolio), container);
 import React from "react";
 import ReactDOM from "react-dom";
-
+ 
 import App from "./App";
  
-class Portfolio extends React.Component {
-  render() {
-    return (
-      <div>
-        <App />
-      </div>
-    );
-  }
-}
-
-// Find the root of our app
-const container = document.getElementsByClassName('app-container')[0];
-
-// Tell React to render our app in the root DOM element
-ReactDOM.render(React.createElement(Portfolio), container);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
