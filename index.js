d3e57ff56a3ae0948ed952e67cc83567077a0df8
wafachaari <wafachaari30@gmail.com>
2021-07-07 11:17:58 +0200

    updated

--- index.js
+++ index.js
@@ -22,12 +22,10 @@
 // ReactDOM.render(React.createElement(Portfolio), container);
 import React from "react";
 import ReactDOM from "react-dom";
- 
 import App from "./App";
- 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById("root")
 );
